16b13a551f2147ce68b65bbb7ec9172a674809f4
Springhawk27 <sajjad0mahmud@gmail.com>
2022-04-11 13:55:06 +0600

    fake data created

---
+++ src/FakeDate.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: "Sajjad Mahmud",
+        username: "sajjad27",
+    },
+    {
+        id: 2,
+        name: "Shaman Istiak",
+        username: "Shaman007",
+    },
+    {
+        id: 3,
+        name: "Shahruk Khan",
+        username: "shahrukh121",
+    },
+    {
+        id: 4,
+        name: "Amir Khan",
+        username: "amir123",
+    },
+    {
+        id: 5,
+        name: "Syed Sadman",
+        username: "sadman234",
+    },
+    {
+        id: 6,
+        name: "Abu Talha",
+        username: "abu_talha",
+    },
+    {
+        id: 7,
+        name: "Salman Khan",
+        username: "salman675",
+    },
+    {
+        id: 8,
+        name: "Karim Elahi",
+        username: "karim456",
+    },
+];
